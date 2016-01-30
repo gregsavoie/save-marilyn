3c9b7025e816b33884de156aa33ffb1142e975a9
Greg Savoie <savg2688@hotmail.com>
2016-01-30 14:47:15 -0500

    implementation d'overlap entre player et marilyn

--- js/main.js
+++ js/main.js
@@ -3,37 +3,45 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game',
 
 var player;
 var cursors;
+var marilyn;
+var score = 0;
 
 function getRandomPosX() {
     var world = new Phaser.World(game);
-    return Math.floor((Math.random() * world._width) + 1);
+    return Math.floor(Math.random() * world._width);
 }
 
 function getRandomPosY() {
     var world = new Phaser.World(game);
-    return Math.floor((Math.random() * world._height) + 1);
+    return Math.floor(Math.random() * world._height);
+}
+
+function saveMarilyn(player, marilyn) {
+    marilyn.reset(getRandomPosX(), getRandomPosY());
 }
 
 function preload() {
-    game.load.image('blue_star', 'img/blue_star.png');
+    game.load.image('marylin', 'img/marylin.png');
     game.load.spritesheet('player', 'img/player.png', 57.5, 75);
 }
 
 function create() {
-    game.add.text(220, 300, "Hollywood Bld OF DRUGS!!", {fill: 'white'});
-
+    game.add.text(220, 300, "Hollywood Bld OF DRUGS!!", {fill: 'white'}); // background
     game.physics.startSystem(Phaser.Physics.ARCADE); // starter la physique
 
+    marilyn = game.add.sprite(getRandomPosX(), getRandomPosY(), 'marylin'); // marilyn random
+    game.physics.arcade.enable(marilyn);
 
     cursors = game.input.keyboard.createCursorKeys(); // linker les touches
 
     /* setter le player... */
     player = game.add.sprite(350, 220, 'player', 1);
     game.physics.arcade.enable(player);
-    player.animations.add('left', [8, 9, 10, 11], 10, true);
-    player.animations.add('right', [4, 5, 6, 7], 10, true);
-    player.animations.add('up', [12, 13, 14, 15], 10, true);
-    player.animations.add('down', [0, 1, 2, 3], 10, true);
+    player.animations.add('left', [8, 9, 10, 11], 20, true);
+    player.animations.add('right', [4, 5, 6, 7], 20, true);
+    player.animations.add('up', [12, 13, 14, 15], 20, true);
+    player.animations.add('down', [0, 1, 2, 3], 20, true);
+    player.body.collideWorldBounds = true;
 }
 
 function update() {
@@ -42,28 +50,28 @@ function update() {
     if (cursors.left.isDown)
     {
         //  Move to the left
-        player.body.velocity.x = -250;
+        player.body.velocity.x = -350;
 
         player.animations.play('left');
     }
     else if (cursors.right.isDown)
     {
         //  Move to the right
-        player.body.velocity.x = 250;
+        player.body.velocity.x = 350;
 
         player.animations.play('right');
     }
     else if (cursors.up.isDown)
     {
         //  Move up
-        player.body.velocity.y = -250;
+        player.body.velocity.y = -350;
 
         player.animations.play('up');
     }
     else if (cursors.down.isDown)
     {
         //  Move down
-        player.body.velocity.y = 250;
+        player.body.velocity.y = 350;
 
         player.animations.play('down');
     }
@@ -72,4 +80,5 @@ function update() {
         //  Stand still
         player.animations.stop();
     }
+    game.physics.arcade.overlap(player, marilyn, saveMarilyn, null, this);
 }
