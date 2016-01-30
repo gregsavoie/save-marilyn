66aaac3c40e42ecede196f59584fc0074ba110e1
Greg Savoie <savg2688@hotmail.com>
2016-01-30 13:21:18 -0500

    ajout du joueur et des controles

--- js/main.js
+++ js/main.js
@@ -1,20 +1,65 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game',
-               {preload: preload, create: create, update: update});
+        {preload: preload, create: create, update: update});
+
+var player;
+var cursors;
 
 function preload() {
     game.load.image('blue_star', 'img/blue_star.png');
+    game.load.spritesheet('player', 'img/player.png', 57.5, 75);
 }
 
 function create() {
-    game.add.text(340, 300, "Hello, world!", {fill: 'white'});
-    var blue_star = game.add.sprite(0,200, 'blue_star');
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    game.physics.arcade.enable(blue_star);    // On démarre le système physique
-    blue_star.enableBody = true;              // Le blue_star est géré dans le système
-    blue_star.body.velocity.x = 200;          // La vitesse horizontale du nuage
-    blue_star.body.bounce.x = 1.0;            // La collision est parfaitement élastique
-    blue_star.body.collideWorldBounds = false; // Collision avec les bords du canvas
+    game.add.text(220, 300, "Hollywood Bld OF DRUGS!!", {fill: 'white'});
+
+    game.physics.startSystem(Phaser.Physics.ARCADE); // starter la physique
+
+
+    cursors = game.input.keyboard.createCursorKeys(); // linker les touches
+
+    /* setter le player... */
+    player = game.add.sprite(0, 0, 'player', 1);
+    game.physics.arcade.enable(player);
+    player.animations.add('left', [8, 9, 10, 11], 10, true);
+    player.animations.add('right', [4, 5, 6, 7], 10, true);
+    player.animations.add('up', [12, 13, 14, 15], 10, true);
+    player.animations.add('down', [0, 1, 2, 3], 10, true);
 }
 
 function update() {
+    player.body.velocity.x = 0;
+    player.body.velocity.y = 0;
+    if (cursors.left.isDown)
+    {
+        //  Move to the left
+        player.body.velocity.x = -250;
+
+        player.animations.play('left');
+    }
+    else if (cursors.right.isDown)
+    {
+        //  Move to the right
+        player.body.velocity.x = 250;
+
+        player.animations.play('right');
+    }
+    else if (cursors.up.isDown)
+    {
+        //  Move up
+        player.body.velocity.y = -250;
+
+        player.animations.play('up');
+    }
+    else if (cursors.down.isDown)
+    {
+        //  Move down
+        player.body.velocity.y = 250;
+
+        player.animations.play('down');
+    }
+    else
+    {
+        //  Stand still
+        player.animations.stop();
+    }
 }
