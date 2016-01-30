a83d056a37b129251ee9233cd1ac24eb13878a7e
Greg Savoie <savg2688@hotmail.com>
2016-01-30 16:08:02 -0500

    implémentation des obstacles, part1

--- js/main.js
+++ js/main.js
@@ -4,8 +4,30 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game',
 var player;
 var cursors;
 var marilyn;
+var pillGroup;
 var score = 0;
 var scoreText;
+var isOdd = true;
+var cmpt = 0
+
+
+function createPill() {
+    var pill;
+    if(cmpt % 9 == 0)
+    {
+        if(isOdd)
+        {
+            pill = pillGroup.create(getRandomPosX(), 0, 'pill');
+            pill.body.velocity.y = 300;
+        }
+        else
+        {
+            pill = pillGroup.create(0, getRandomPosY(), 'pill');
+            pill.body.velocity.x = 300;
+        }
+        pill.body.collideWorldBounds = false;
+    }
+}
 
 function getRandomPosX() {
     var world = new Phaser.World(game);
@@ -25,16 +47,21 @@ function saveMarilyn(player, marilyn) {
 
 function preload() {
     game.load.image('marylin', 'img/marylin.png');
+    game.load.image('pill', 'img/pill.png');
     game.load.spritesheet('player', 'img/player.png', 57.5, 75);
 }
 
 function create() {
     game.add.text(220, 300, "Hollywood Bld OF DRUGS!!", {fill: 'white'}); // background
-    scoreText = game.add.text(8, 8, 'score: 0', {fontSize: '18px', fill: 'white'});
+    scoreText = game.add.text(8, 8, 'score: 0', {fontSize: '18px', fill: 'white'}); // affichage du score;
     game.physics.startSystem(Phaser.Physics.ARCADE); // starter la physique
 
     marilyn = game.add.sprite(getRandomPosX(), getRandomPosY(), 'marylin'); // marilyn random
     game.physics.arcade.enable(marilyn);
+
+    pillGroup = game.add.group();
+    game.physics.arcade.enable(pillGroup);
+    pillGroup.enableBody = true;
 
     cursors = game.input.keyboard.createCursorKeys(); // linker les touches
 
@@ -49,6 +76,9 @@ function create() {
 }
 
 function update() {
+    //game.add.text(50, 50. 'allo', {fill: 'white'});
+    isOdd = !isOdd;
+    cmpt++;
     player.body.velocity.x = 0;
     player.body.velocity.y = 0;
     if (cursors.left.isDown)
@@ -57,6 +87,7 @@ function update() {
         player.body.velocity.x = -350;
 
         player.animations.play('left');
+        createPill();
     }
     else if (cursors.right.isDown)
     {
@@ -64,6 +95,7 @@ function update() {
         player.body.velocity.x = 350;
 
         player.animations.play('right');
+        createPill();
     }
     else if (cursors.up.isDown)
     {
@@ -71,6 +103,7 @@ function update() {
         player.body.velocity.y = -350;
 
         player.animations.play('up');
+        createPill();
     }
     else if (cursors.down.isDown)
     {
@@ -78,6 +111,7 @@ function update() {
         player.body.velocity.y = 350;
 
         player.animations.play('down');
+        createPill();
     }
     else
     {
